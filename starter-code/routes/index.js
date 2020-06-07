386adcb3622f4c7ed024805a326d57c7a1cf5ba9
Ana Segarra <anasegarraber@gmail.com>
2020-06-07 11:56:10 +0200

    Cleanup code

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const celebrities = require('./celebrities');
+const movies = require('./movies');
+
+/* use celebrities and movie routers */
+router.use('/celebrities', celebrities);
+router.use('/movies', movies);
 
 /* GET home page */
 router.get('/', (req, res, next) => {
